cbadfe5b60ae9132dfbfedba441d8ffd9fd6d73c
MarlonAnacona <marlon.andres.anacona@gmail.com>
2023-10-03 21:32:02 -0500

    dinamica terminado, falta acomodar respuesta recibidas

--- Algoritmos/dinamica.py
+++ Algoritmos/dinamica.py
@@ -14,8 +14,6 @@ def inicio(nombre_archivo):
 
 def rocPD(k, r, M, E):
 
-    #asignacionMaterias(convertirMateriasACupos(M),    convertirEstudiantesMateriasVectores(E,M))
-    nuevo_estudiantes={}
     estudiantes=convertirEstudiantesMateriasVectores(E,M)
     cupos = list(M.values())
     print(asignar_materia(estudiantes,cupos))
@@ -32,42 +30,9 @@ def convertirEstudiantesMateriasVectores(E,M):
             indice = list(M.keys()).index(materia)
             matriz_estudiante[indice] = valor
         matrices_estudiantes[codigo] = matriz_estudiante
-
-    # Imprimir las matrices de los estudiantes
-  for codigo, matriz in matrices_estudiantes.items():
-        print(f"Estudiante {codigo}: {matriz}")
   return matrices_estudiantes
         
-        
-def convertirMateriasACupos(materias):
-    claves = list(materias.keys())
-    valores = list(materias.values())
-    return cuposUnicos(claves,valores)
-    
-def cuposUnicos(claves,valores):
-    contadores = [0] * len(claves)
-    
-    # Inicializa una matriz vacía para almacenar las combinaciones
-    
-    # Bucle para generar todas las combinaciones posibles
-    while True:
-        # Añade la combinación actual a la matriz
-        matriz_combinaciones.append(list(contadores))  # Añade una copia de los contadores actuales
-    
-        # Incrementa los contadores
-        for i in range(len(contadores) - 1, -1, -1):
-            if contadores[i] < valores[i]:
-                contadores[i] += 1
-                break
-            else:
-                contadores[i] = 0
-    
-        # Verifica si todos los contadores han vuelto a 0, lo que indica que todas las combinaciones posibles se han generado
-        if all(c == 0 for c in contadores):
-            break
-        
-    return matriz_combinaciones
-    
+
 def obtenerCombinacionDeMatriz(matriz_combinaciones, indice):
     try:
         return matriz_combinaciones[indice]  # Retorna la combinación en la posición dada.
@@ -97,111 +62,53 @@ def calcularmateriasAsignadasYSolicitadas (prioridades):
             suma += elemento
     return solicitadas,asignadas,suma
    
-   
-def matricular(cupos, solicitudes, materia=0, estado=None):
-    if estado is None:
-        estado = {est: list(valores) for est, valores in solicitudes.items()}  # Inicialización con valores iniciales
-
-    if materia == len(cupos):
-        yield copy.deepcopy(estado)
-        return
-
-    estudiantes_posibles = [e for e in solicitudes.keys()]
-    for estudiante in estudiantes_posibles:
-        if cupos[materia] > 0 and estado[estudiante][materia] != -1:  # Usando estado en lugar de valores_iniciales
-            cupos[materia] -= 1
-            valor_original = estado[estudiante][materia]  # Guardar el valor original
-            estado[estudiante][materia] = 0  # Matriculado
-            yield from matricular(cupos, solicitudes, materia, estado)
-            cupos[materia] += 1
-            estado[estudiante][materia] = valor_original  # Restaurar el valor original
-
-    yield from matricular(cupos, solicitudes, materia + 1, estado)
-
-   
-def asignacionMaterias(materias, estudiantes):
-    matriz_insatisfacciones = []  # Matriz para almacenar la insatisfacción para cada combinación
-    estudiantes_acumulados = {}
-    insatisfaccion_promedio=0
-    print (estudiantes)
-    for indice in range(len(materias)):
-        estudiantes_acumulados.clear()
-        fila = obtenerCombinacionDeMatriz(materias, indice)
-        for estudiante, valores in estudiantes.items():
-            estudiantes_acumulados.update({estudiante:valores})
-            if fila is not None:
-                total_insatisfaccion = 0
-                total_estudiantes = 0
-                menorPromedio=[]
-                combinaciones=list(matricular(fila,estudiantes_acumulados))
-                for i, combinacion in enumerate(combinaciones, 1):
-                    total_insatisfaccion = 0
-                    total_estudiantes = 0
-                    for estudiante, materiasEstudiante in combinacion.items():
-                        insatisfaccion = calcularInsatisfaccion(materiasEstudiante)
-                        total_insatisfaccion += insatisfaccion
-                        total_estudiantes += 1
-                    insatisfaccion_promedio = total_insatisfaccion / total_estudiantes
-                    menorPromedio.append(insatisfaccion_promedio)
-                print(min(menorPromedio))
-            matriz_insatisfacciones.append(insatisfaccion_promedio)
-
-
-def calcular_insatisfaccion(asignaciones, estudiantes):
-    ins_general = 0
-    num_estudiantes = len(estudiantes)
-    for estudiante, materias in zip(estudiantes, asignaciones):
-       ins_general+=calcularInsatisfaccion(materias) 
-    return ins_general / num_estudiantes
 
 memo = {}  # Diccionario para almacenar resultados previos
 
-
+respuesta=[]
 
 def asignar_materia(estudiante, vectorCupos, memo={}):
 
     if len(estudiante) == 0:
-        return 0
+        return 0, []
     
     last_key = next(reversed(estudiante.keys()))
     last_value = estudiante[last_key]
 
-    # Usar tupla para que sea hashable y se pueda usar como clave en dict
     key = (last_key, tuple(vectorCupos)) 
 
-    # Chequear si el resultado ya fue calculado
     if key in memo:
         return memo[key]
     
     if sum(vectorCupos) == 0:
-        return 1
-    
+        return 1, []
 
-    
-   
     estudianteCopy = estudiante.copy()
-    last_item=estudianteCopy.popitem()
+    estudianteCopy.popitem()
+
+    ins = []
+    caminos_completos = []
+
     for camino in calcularCaminosPosibles(last_value, vectorCupos):
         result = restar_unidad_si_condicion(camino, vectorCupos)
-        ins = []
 
-        if(len(estudiante)>0):
-            if(len(estudiante)==1):
-                insG = (
-                    calcularInsatisfaccion(camino)
-                    + ((asignar_materia(estudianteCopy, result, memo)) * (len(estudiante)))
-                ) / len(estudiante)
-            else:
-                insG = (
-                    calcularInsatisfaccion(camino)
-                    + ((asignar_materia(estudianteCopy, result, memo)) * (len(estudiante)-1))
-                ) / len(estudiante)
-            ins.append(insG)
+        ins_actual, camino_anterior = asignar_materia(estudianteCopy, result, memo)
         
-    
-    # Guardar el resultado en el diccionario antes de retornar
-    memo[key] = min(ins)
-    return min(ins)
+        if len(estudiante) == 1:
+            insG = (calcularInsatisfaccion(camino) + (ins_actual * len(estudiante))) / len(estudiante)
+        else:
+            insG = (calcularInsatisfaccion(camino) + (ins_actual * (len(estudiante)-1))) / len(estudiante)
+
+        ins.append(insG)
+        caminos_completos.append([camino] + camino_anterior)  # Añadir el camino actual al camino anterior
+        
+    min_ins = min(ins)
+    min_index = ins.index(min_ins)
+
+    # Guardar el resultado en memo
+    memo[key] = (min_ins, caminos_completos[min_index])
+    return memo[key]
+
 
     
 def calcularCaminosPosibles(estudiante,cupos):
@@ -219,7 +126,8 @@ def calcularCaminosPosibles(estudiante,cupos):
 def restar_unidad_si_condicion(vector1, vector2):
     """
     Esta función recibe dos vectores y, para cada posición i, 
-    si vector1[i] > 0 y vector2[i] > 0, resta 1 a vector2[i].
+    si vector1[i] es 0, resta 1 a vector2[i]. Si vector1[i] es diferente de 0,
+    no modifica vector2[i].
 
     :param vector1: list, primer vector para chequear la condición.
     :param vector2: list, segundo vector para restar las unidades.
@@ -233,43 +141,27 @@ def restar_unidad_si_condicion(vector1, vector2):
     result_vector = vector2.copy()
     
     for i in range(len(vector1)):
-        # Si el elemento en vector1 es mayor que 0...
-        if vector1[i] > 0:
+        # Si el elemento en vector1 es 0...
+        if vector1[i] == 0:
             # ...y el elemento correspondiente en result_vector es mayor que 0...
             if result_vector[i] > 0:
                 # ...resta 1 al elemento en result_vector.
                 result_vector[i] -= 1
-            # Si el elemento en result_vector ya es 0, lo mantiene como está
+            # Si el elemento en result_vector ya es 0 o negativo, lo mantiene como está
             else:
-                result_vector[i] = 0
+                result_vector[i] = result_vector[i]
+        # Si el elemento en vector1 es diferente de 0, no modifica result_vector[i]
+        else:
+            result_vector[i] = result_vector[i]
     
     return result_vector
 
-estudiante={
-     1000: [3, 2, -1],
- 1001: [1, 2, 5],
-     1003: [3, -1, 2],
-}
-print(asignar_materia(estudiante,[2,0,0]))
+# estudiante={
+#      1000: [3, 2, -1],
+#  1001: [1, 2, 5],
+#      1003: [3, -1, 2],
+# }
+# print(asignar_materia(estudiante,[2,0,0]))
 
 
-#inicio("./Pruebas/e_3_5_5.txt")
-
-
-    
-    # for i, sublista in enumerate(vectorCupos):
-    #     for j, elemento in enumerate(sublista):
-    #         if elemento > 0 and estudiantes[est_index][j] != -1:
-    #             materias[i] -= 1
-    #             asignaciones_copy[est_index][i] = 0
-    #             #En caso de que matricule
-    #             if(i-1<0):
-    #                ins.append(asignar_materia(estudiantes, asignaciones_copy,vectorCupos, est_index -1,vectorCupos[i])) 
-    #             else:
-    #                 ins.append(asignar_materia(estudiantes, asignaciones_copy,vectorCupos, est_index -1,vectorCupos[i-1]))    
-                
-    #             materias[i] += 1
-    #             asignaciones_copy[est_index][i] = estudiantes[est_index][i]
-    #         else:
-    #             #En caso de que no
-    #             ins.append(asignar_materia(estudiantes, asignaciones_copy,vectorCupos, est_index -1,materias))
+inicio("./Pruebas/e_3_5_5.txt")
