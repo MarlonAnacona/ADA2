052e5301e0d522dca680f453fb954f298f823f29
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2023-10-04 23:50:45 -0500

    me faltaba guardar

--- Algoritmos/voraz.py
+++ Algoritmos/voraz.py
@@ -45,40 +45,11 @@ def rocV(total_subjects, total_student, subjects, requests):
   
   return [answer, dissatisfaction]
 
-def readFile(file):
-  with open(file, 'r') as entry:
-    total_subjects = int(entry.readline())
-    subjects = {}
-    requests = {}
 
-    for i in range(0, total_subjects):
-      subject = entry.readline()
-      subject = subject.split(",")
-      subjects[subject[0]] = int(subject[1].strip())
 
-    total_student = int(entry.readline())
+# file = './Pruebas/e_3_5_5.roc'
 
-    for i in range(0, total_student):
-      
-      student = entry.readline()
-      student = student.split(",")
+# k, r, M, E = readFile(file)
+# voraz = rocV(k, r, M, E)
 
-      new_student = {}
-      student_subject = int(student[1].strip())
-      
-      for j in range (0, student_subject):
-        requested_subject = entry.readline()
-        requested_subject = requested_subject.split(",")
-        new_student[requested_subject[0]] = int(requested_subject[1].strip())
-
-      requests[student[0]] = new_student
-
-    entry.close()
-    return total_subjects, total_student, subjects, requests
-
-file = './Pruebas/e_3_5_5.roc'
-
-k, r, M, E = readFile(file)
-voraz = rocV(k, r, M, E)
-
-print(voraz)
+# print(voraz)
